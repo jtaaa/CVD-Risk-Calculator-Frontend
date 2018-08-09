1844c20ce2bbf64320af7616d415277b8121a8bb
jtaaa <joshuatallum@gmail.com>
2018-08-09 10:35:42 -0400

    Add page logic and submit page

--- src/App.js
+++ src/App.js
@@ -10,19 +10,32 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    this.state = { page: 0 }
+
+    this.toFormPage          = this.toFormPage.bind(this);
+    this.toSubmittedPage     = this.toSubmittedPage.bind(this);
     this.setScore            = this.setScore.bind(this);
     this.setScoreAndToSubmit = this.setScoreAndToSubmit.bind(this);
   }
 
-  setScore(score)            { this.setState({ score }); }
-  setScoreAndToSubmit(score) { this.setScore(score); }
+  toFormPage()               { this.setState({ page: 0 }); }
+  toSubmittedPage()          { this.setState({ page: 1 }); }
+  setScore(score)            { this.setState({ score   }); }
+  setScoreAndToSubmit(score) { this.setScore(score); this.toSubmittedPage(); }
 
   render() {
     return (
       <div className="App">
-        <InfoForm 
+        {this.state.page === 0 &&
+        <InfoForm
+            header="Cardiovascular Disease Risk Calculator" 
             fields={ fields }
-            onSubmit={ data => getScore(data, true).then(this.setScoreAndToSubmit(data)) } />
+            onSubmit={ data => getScore(data, true).then(this.setScoreAndToSubmit(data)) } />}
+        {this.state.page === 1 &&
+        <div>
+          <div className="score-message">{ this.state.score.message }</div>
+          <div onClick={ this.toFormPage }>Back</div>
+        </div>}
       </div>
     );
   }
