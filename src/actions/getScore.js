02468e37358e8a4caaf43b2a51a094aabc70fc69
jtaaa <joshuatallum@gmail.com>
2018-08-09 10:26:19 -0400

    Add getScore function

--- src/actions/getScore.js
+++ src/actions/getScore.js
@@ -2,8 +2,8 @@ import Axios from "axios";
 
 const SCORE_URL = 'https://fast-inlet-87027.herokuapp.com/api/score';
 
-const getScore = async res => { 
-  return Axios.post(SCORE_URL, res);
+const getScore = async (res, save) => { 
+  return Axios.post(SCORE_URL, { ...res, save });
 };
 
 export default getScore;
