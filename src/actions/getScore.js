e9374ff91970506c92cf95ee8ca578a65b2e9f96
jtaaa <joshuatallum@gmail.com>
2018-08-29 22:17:22 -0400

    Use glitch app endpoint

--- src/actions/getScore.js
+++ src/actions/getScore.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-const SCORE_URL = 'https://fast-inlet-87027.herokuapp.com/api/score';
+const SCORE_URL = 'https://lake-barber.glitch.me/api/score';
 
 const getScore = async (res, save) => { 
   return Axios.post(SCORE_URL, { ...res, save });
